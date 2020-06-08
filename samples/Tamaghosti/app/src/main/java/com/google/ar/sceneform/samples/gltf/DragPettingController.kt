1488c57c8c88e9b6af17d25ba39fc5cd0d1b65a4
Florian Buchholz <33148960+masterbuchi@users.noreply.github.com>
2020-06-08 22:44:45 +0200

    Petting fixed

--- samples/Tamaghosti/app/src/main/java/com/google/ar/sceneform/samples/gltf/DragPettingController.kt
+++ samples/Tamaghosti/app/src/main/java/com/google/ar/sceneform/samples/gltf/DragPettingController.kt
@@ -13,25 +13,16 @@ class DragPettingController(private var dragon: Dragon, transformableNode: BaseT
 
 
     public override fun canStartTransformation(gesture: DragGesture): Boolean {
+        return transformableNode.isSelected
+    }
+
+    public override fun onContinueTransformation(gesture: DragGesture) {
         if (!animationStart) {
             dragon.updateAnimation(dragon.getPet_index);
             animationStart = true
             Log.d("Petting", "Petting started")
         }
 
-        return transformableNode.isSelected
-    }
-
-    public override fun onContinueTransformation(gesture: DragGesture) {
-
-
-       /* var localRotation = transformableNode.localRotation
-
-        val rotationAmountX = gesture.delta.x * rotationRateDegrees
-        val rotationDeltaX = Quaternion(Vector3.up(), rotationAmountX)
-        localRotation = Quaternion.multiply(localRotation, rotationDeltaX)
-
-        transformableNode.localRotation = localRotation*/
     }
 
     public override fun onEndTransformation(gesture: DragGesture) {
@@ -43,6 +34,8 @@ class DragPettingController(private var dragon: Dragon, transformableNode: BaseT
             Log.d("Petting", "Petting finished")
         }
     }
+
+
 
 
 }
