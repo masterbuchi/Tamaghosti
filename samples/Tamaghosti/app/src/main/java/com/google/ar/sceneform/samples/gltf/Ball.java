5d3b9908e20a0958459cbec009c393d067e19d2e
Florian Buchholz <33148960+masterbuchi@users.noreply.github.com>
2020-06-24 00:46:22 +0200

    Update Ball.java

--- samples/Tamaghosti/app/src/main/java/com/google/ar/sceneform/samples/gltf/Ball.java
+++ samples/Tamaghosti/app/src/main/java/com/google/ar/sceneform/samples/gltf/Ball.java
@@ -1,7 +1,6 @@
 package com.google.ar.sceneform.samples.gltf;
 
 import android.animation.ObjectAnimator;
-import android.util.Log;
 import android.view.animation.LinearInterpolator;
 
 import com.google.ar.core.Anchor;
@@ -15,15 +14,19 @@ import com.google.ar.sceneform.math.Vector3Evaluator;
 import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
+/**
+ * Class for the Ball Object
+ */
 public class Ball extends Node {
 
-    ArFragment arFragment;
-    Renderable renderable;
-    Control control;
-    ObjectAnimator ballRotationAnimation;
+    private ArFragment arFragment;
+    private Renderable renderable;
+    private Control control;
+    private ObjectAnimator ballRotationAnimation;
 
     /**
      * Constructor Ball-object
+     *
      * @param arFragment
      * @param renderable
      * @param control
@@ -54,71 +57,93 @@ public class Ball extends Node {
     }
 
     /**
+     * Sets ballparent to Hitpoint on plane and starts Ball-Throw-Animation
      *
      * @param hitResult
      */
     void ballAnimation(HitResult hitResult) {
 
-
+        // creates anchor and node and sets its parent to Scene
         Anchor anchor = hitResult.createAnchor();
-
         AnchorNode anchorNode = new AnchorNode(anchor);
         anchorNode.setParent(arFragment.getArSceneView().getScene());
 
         Vector3 oldPosition = getWorldPosition();
         Vector3 newPosition = anchorNode.getWorldPosition();
 
+        // Tell Spectator current Positions and the wanted new Position
         control.updatePositions(newPosition);
 
-        newPosition=new Vector3().add(newPosition,new Vector3(0,0.1f,0));
+        // Change height of new Position to compensate the pivotpoint in the middle of the ball (estimated height)
+        newPosition = new Vector3().add(newPosition, new Vector3(0, 0.1f, 0));
 
+        // Direction vector in the direction of the new Position
         Vector3 directionVector = new Vector3().subtract(newPosition, oldPosition);
 
-        Vector3 middlePosition = Vector3.add(oldPosition,directionVector.scaled(0.5f));
+        // Position in the middle of the throw, used as highest point (x and z coordinates) of the parableThrow
+        Vector3 middlePosition = Vector3.add(oldPosition, directionVector.scaled(0.5f));
 
+        // Set Parent of Ball to anchorNode
         setParent(anchorNode);
 
-        throwAnimation(oldPosition,newPosition,directionVector,middlePosition);
+        // Start throw Animation with calculated Vectors
+        throwAnimation(oldPosition, newPosition, directionVector, middlePosition);
 
     }
 
 
-    // Spectator version
-
+    /**
+     * Spectator Version
+     * Sets ballparent to Hitpoint on plane and starts Ball-Throw-Animation
+     *
+     * @param newPosition
+     * @param oldPosition
+     */
     void ballAnimation(Vector3 newPosition, Vector3 oldPosition) {
 
+        // Since for the Spectator the ball isn't visible, set the renderable visible (not null)
         setRenderable(renderable);
+
+        // Create AnchorNode, set its Position to the NewPosition-Vector, that is sent by the Creator
+        // also set Parent to Scene
         AnchorNode anchorNode = new AnchorNode();
-
         anchorNode.setWorldPosition(newPosition);
-
         anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-        newPosition = new Vector3().add(newPosition,new Vector3(0,0.1f,0));
+        // Change height of new Position to compensate the pivotpoint in the middle of the ball (estimated height)
+        newPosition = new Vector3().add(newPosition, new Vector3(0, 0.1f, 0));
 
+        // Direction vector in the direction of the new Position
         Vector3 directionVector = new Vector3().subtract(newPosition, oldPosition);
 
-        Vector3 middlePosition = Vector3.add(oldPosition,directionVector.scaled(0.5f));
+        // Position in the middle of the throw, used as highest point (x and z coordinates) of the parableThrow
+        Vector3 middlePosition = Vector3.add(oldPosition, directionVector.scaled(0.5f));
 
+        // Set Parent of Ball to anchorNode
         setParent(anchorNode);
 
-        throwAnimation(oldPosition,newPosition,directionVector,middlePosition);
+        // Start throw Animation with calculated Vectors
+        throwAnimation(oldPosition, newPosition, directionVector, middlePosition);
 
     }
 
     void throwAnimation(Vector3 oldPosition, Vector3 newPosition, Vector3 directionVector, Vector3 middlePosition) {
 
-
-
-
+        // Stop Animation (In Creator the Rotation, when activated but not thrown)
         stopAnimation();
+
+        // Start Animation of Rotation of the Ball
         startAnimation(true);
+
+        // Sets Starting-Numbers
         float x_1 = oldPosition.x;
         float x_2 = newPosition.x;
         float x_3 = middlePosition.x;
         float y_1 = oldPosition.y;
         float y_2 = newPosition.y;
-        float y_3 = middlePosition.y+directionVector.scaled(0.2f).length();
+
+        // Adds 20% of the throwing-length as height of the parable
+        float y_3 = middlePosition.y + directionVector.scaled(0.2f).length();
         float x;
         float y;
         float z;
@@ -126,47 +151,56 @@ public class Ball extends Node {
 
         float distance = directionVector.length();
 
+        //interestingly enough its exactly one to look good
         double velocity = 1;
+
+        //calculate throwing time
         long time = (long) ((distance / velocity) * 1000);
 
 
         int steps = 200;
 
+        // Array of position of the parableThrow
         Vector3[] positions = new Vector3[steps];
 
+        // current ball position is the starting point
         Vector3 currentPos = oldPosition;
 
 
         // 200 Steps for smooth curve
         for (int i = 0; i < steps; i++) {
 
+            // add 1/200 of the length of the  to the current position
             x = currentPos.x + directionVector.x / (float) steps;
 
-            y  = (x*y_2 - x*y_3 + x_2*y_3 - x_3*y_2)/(x_2 - x_3) + ((x - x_2)*(x - x_3)*(y_1 - y_2))/((x_1 - x_2)*(x_2 - x_3)) - ((x - x_2)*(x - x_3)*(y_1 - y_3))/((x_1 - x_3)*(x_2 - x_3));
+            // very complicated of the y-Position that of course I did by hand and not with Matlab ;)
+            y = (x * y_2 - x * y_3 + x_2 * y_3 - x_3 * y_2) / (x_2 - x_3) + ((x - x_2) * (x - x_3) * (y_1 - y_2)) / ((x_1 - x_2) * (x_2 - x_3)) - ((x - x_2) * (x - x_3) * (y_1 - y_3)) / ((x_1 - x_3) * (x_2 - x_3));
 
+            // add 1/200 of the length of the  to the current position
             z = currentPos.z + directionVector.z / (float) steps;
 
+            // set currentPos
+            currentPos = new Vector3(x, y, z);
 
-            currentPos = new Vector3(x, y,z);
-
+            // add currentPos to the array
             positions[i] = currentPos;
 
         }
 
+        // needed for Spectators
         setWorldPosition(oldPosition);
-
         setLocalRotation(new Quaternion(0, 180, 180, 0));
         setLocalScale(new Vector3(0.1f, 0.1f, 0.1f));
 
+
+        // Set the specs for the Movement-Animation
         ObjectAnimator objectAnimation = new ObjectAnimator();
         objectAnimation.setAutoCancel(true);
         objectAnimation.setTarget(this);
         objectAnimation.setObjectValues(positions);
         objectAnimation.setPropertyName("WorldPosition");
-        // The Vector3Evaluator is used to evaluator 2 vector3 and return the next
-        // vector3.  The default is to use lerp.
         objectAnimation.setEvaluator(new Vector3Evaluator());
-        // This makes the animation linear (smooth and uniform).
+        // Linear Movement, not exactly physically correct, but looks okay
         objectAnimation.setInterpolator(new LinearInterpolator());
 
         // Duration in ms of the animation.
@@ -180,31 +214,38 @@ public class Ball extends Node {
             }
 
             @Override
+            // When the Animation is finished it will check if it should bounce again
             public void onAnimationEnd(android.animation.Animator animation) {
 
+                // calculate new vectors (half of the distance before with fitted direction
                 Vector3 oldPosition = getWorldPosition();
-                Vector3 newdirectionVector = new Vector3(directionVector.scaled(0.5f).x,0,directionVector.scaled(0.5f).z);
-                Vector3 newPosition = Vector3.add(oldPosition,newdirectionVector);
+                Vector3 newdirectionVector = new Vector3(directionVector.scaled(0.5f).x, 0, directionVector.scaled(0.5f).z);
+                Vector3 newPosition = Vector3.add(oldPosition, newdirectionVector);
                 Vector3 directionVector = new Vector3().subtract(newPosition, oldPosition);
+                Vector3 middlePosition = Vector3.add(oldPosition, directionVector.scaled(0.5f));
 
-
-                Vector3 middlePosition = Vector3.add(oldPosition,directionVector.scaled(0.5f));
-
+                // when the new distance is still long enough to bounce
                 if (directionVector.length() > 0.05) {
-                    throwAnimation(oldPosition,newPosition,directionVector,middlePosition);
+                    throwAnimation(oldPosition, newPosition, directionVector, middlePosition);
                 } else {
 
+                    // set the position, where the dragon should go to
                     Vector3 dragonPosition = control.getDragon().getWorldPosition();
                     Vector3 rotationVect = new Vector3().subtract(oldPosition, dragonPosition);
-                    double distance = Math.sqrt(Math.pow(dragonPosition.x - oldPosition.x, 2) + Math.pow(dragonPosition.y - oldPosition.y, 2) + Math.pow(dragonPosition.z - oldPosition.z, 2));
 
-                    Vector3 moveToPosition=new Vector3().add(oldPosition,new Vector3(0,-0.1f,0));
+                    // get distance
+                    double distance = rotationVect.length();
 
+                    // set Position to go to minus the previous added y-change of the ball
+                    Vector3 moveToPosition = new Vector3().add(oldPosition, new Vector3(0, -0.1f, 0));
+
+                    // Start Movement of the Dragon
                     long time = control.getDragon().moveTo(moveToPosition, distance, rotationVect);
 
+                    // start Thread that starts other actions after Picking up ball
                     control.startThread((float) time);
 
-
+                    // stop ball rotation of the Throw
                     stopAnimation();
                 }
 
@@ -223,53 +264,61 @@ public class Ball extends Node {
 
     }
 
-
+    /**
+     * Start the Rotation-Animation of the Ball
+     *
+     * @param wurf
+     */
     void startAnimation(boolean wurf) {
+
+        // if Animation is already running return
         if (ballRotationAnimation != null) {
             return;
         }
 
+        // Start Ballrotation
         ballRotationAnimation = createAnimator(wurf);
         ballRotationAnimation.setTarget(this);
         ballRotationAnimation.setDuration(1000 * 360 / 90);
         ballRotationAnimation.start();
     }
 
+    /**
+     * Stop BallRotationAnimation
+     */
     void stopAnimation() {
+        // if no Animation running, return
         if (ballRotationAnimation == null) {
             return;
         }
+        // cancel and set null
         ballRotationAnimation.cancel();
         ballRotationAnimation = null;
     }
 
-    /** Returns an ObjectAnimator that makes this node rotate. */
+    /**
+     * Returns an ObjectAnimator that makes the ball rotate.
+     * Some of the code is from the ARCORE-Example of the Solar Star System - Planet Rotation.
+     */
     private static ObjectAnimator createAnimator(boolean wurf) {
-        // Node's setLocalRotation method accepts Quaternions as parameters.
-        // First, set up orientations that will animate a circle.
+
+        // set the rotationsPoints of the Animation
         Quaternion[] orientations = new Quaternion[4];
-        // Rotation to apply first, to tilt its axis.
-        Quaternion baseOrientation = Quaternion.eulerAngles(new Vector3(0f, 0, 0f));
+
+        // Calculate the rotation-positions
         for (int i = 0; i < orientations.length; i++) {
             float angle = i * 360 / (orientations.length - 1);
             Quaternion orientation;
-if (wurf) orientation = Quaternion.axisAngle(new Vector3(1f, 0f, 0f), angle);
-else      orientation = Quaternion.axisAngle(new Vector3(0f, 1f, 0f), angle);
-
-            orientations[i] = Quaternion.multiply(baseOrientation, orientation);
+            // if the Ball is thrown set the Rotation around the x-axis, otherwise set it around y-axis
+            if (wurf) orientation = Quaternion.axisAngle(new Vector3(1f, 0f, 0f), angle);
+            else orientation = Quaternion.axisAngle(new Vector3(0f, 1f, 0f), angle);
+            orientations[i] = orientation;
         }
 
         ObjectAnimator orbitAnimation = new ObjectAnimator();
-        // Cast to Object[] to make sure the varargs overload is called.
         orbitAnimation.setObjectValues(orientations);
-
-        // Next, give it the localRotation property.
         orbitAnimation.setPropertyName("localRotation");
-
-        // Use Sceneform's QuaternionEvaluator.
         orbitAnimation.setEvaluator(new QuaternionEvaluator());
-
-        //  Allow orbitAnimation to repeat forever
         orbitAnimation.setRepeatCount(ObjectAnimator.INFINITE);
         orbitAnimation.setRepeatMode(ObjectAnimator.RESTART);
         orbitAnimation.setInterpolator(new LinearInterpolator());
